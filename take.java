9460eefbe152ce2cef1ac0b5c0deab396f913fb4
Olga Belianko <Obelyanko@gmail.com>
2021-05-29 19:07:11 +0300

    take.java update

--- take.java
+++ take.java
@@ -1,19 +1,18 @@
 public class Main {
-public static void main(String[] args) {
-    int accountBalance = 100;
-    int topUpBalance = 1000;
-    int bonus = topUpBalance / 100;
+    public static void main(String[] args) {
+        int accountBalance = 100;
+        int topUpBalance = 1100;
+        int bonus;
 
-    if (topUpBalance >= 1000)
-    { System.out.println("Начисленные бонусы:" + bonus);
-
-    } else {
-        bonus = 0;
-        System.out.println("Начисленные бонусы:" + 0);
-    }
-
-    int totalBalance = accountBalance + topUpBalance + bonus;
-    System.out.println("Балланс счета:" + totalBalance);
+        if (topUpBalance >= 1000) {
+            bonus = topUpBalance / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Начисленные бонусы:" + bonus);
+        int totalBalance = accountBalance + topUpBalance + bonus;
+        System.out.println("Балланс счета:" + totalBalance);
 
     }};
+
 
