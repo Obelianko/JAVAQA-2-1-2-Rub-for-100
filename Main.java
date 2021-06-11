c6ac63c4e116e56798992a3a010764bea917636c
Olga Belianko <Obelyanko@gmail.com>
2021-06-11 12:26:14 +0300

    rename main.java code update

--- Main.java
+++ Main.java
@@ -1,18 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         int accountBalance = 100;
-        int topUpBalance = 1100;
-        int bonus;
+        int topUpBalance = 1000;
+        int bonus = topUpBalance / 100;
 
-        if (topUpBalance > 1000) {
-            bonus = topUpBalance / 100;
+        if (topUpBalance >= 1000) {
+            System.out.println("Начисленные бонусы:" + bonus);
+
         } else {
             bonus = 0;
+            System.out.println("Начисленные бонусы:" + 0);
         }
-        System.out.println("Начисленные бонусы:" + bonus);
+
         int totalBalance = accountBalance + topUpBalance + bonus;
         System.out.println("Балланс счета:" + totalBalance);
 
-    }};
+    }
+}
 
 
