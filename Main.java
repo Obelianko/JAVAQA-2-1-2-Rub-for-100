900590bdbb315ca86b354846f76dc98c5935493f
Olga Belianko <Obelyanko@gmail.com>
2021-06-19 23:17:42 +0300

    Main.java code update

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int accountBalance = 100;
-        int topUpBalance = 1000;
-        int bonus = topUpBalance / 100;
+        int topUpBalance = 1100;
+        int bonus;
 
         if (topUpBalance > 1000) {
-            System.out.println("Начисленные бонусы:" + bonus);
-
+            bonus = topUpBalance / 100;
         } else {
             bonus = 0;
-            System.out.println("Начисленные бонусы:" + 0);
         }
-
+        System.out.println("Начисленные бонусы:" + bonus);
         int totalBalance = accountBalance + topUpBalance + bonus;
         System.out.println("Балланс счета:" + totalBalance);
 
